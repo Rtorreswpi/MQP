f1d2041c388dc81e8f73d450db192bc13d9e7426
Rtorreswpi <Rtorres@wpi.edu>
2026-01-28 00:23:03 -0500

    added left and right  dirrection throttle

--- test.py
+++ test.py
@@ -6,9 +6,13 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=8)
 kit.continuous_servo[0].throttle=0
 kit.continuous_servo[1].throttle=0
+throt=0
 time.sleep(1)
 while True:
-    kit.continuous_servo[1].throttle = 1
-    time.sleep(2)
-    kit.continuous_servo[1].throttle = -1
-    time.sleep(2)
+    dir=input("pick a direction(l or r): ")
+    if dir == "l": throt=-1
+    elif dir =="r":throt=1
+    else:
+        print("incorrect direction try again")
+        throt=0
+    kit.continuous_servo[1].throttle = throt
