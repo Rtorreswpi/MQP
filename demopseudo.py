daeec89c7e37eb21c76e1bf007b45f40de749c37
Rtorreswpi <Rtorres@wpi.edu>
2026-03-11 17:16:00 -0400

    added descriptions and fixed pins to be accurate

--- demopseudo.py
+++ demopseudo.py
@@ -7,15 +7,15 @@ import functions as f
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
 kit = ServoKit(channels=16)
 #finger servos
-finger1=kit.continuous_servo[0]
+finger1=kit.continuous_servo[0]#pinky
 finger1.throttle=0
-finger2=kit.continuous_servo[1]
+finger2=kit.continuous_servo[1]#ring
 finger2.throttle=0
-finger3=kit.continuous_servo[2]
+finger3=kit.continuous_servo[2]#middle
 finger3.throttle=0
-finger4=kit.continuous_servo[3]
+finger4=kit.continuous_servo[3]#pointer
 finger4.throttle=0
-finger5=kit.continuous_servo[4]
+finger5=kit.continuous_servo[4]#thumb
 finger5.throttle=0
 #wrist servos
 wristRight=kit.continuous_servo[5]
@@ -31,31 +31,34 @@ elbow.throttle=0
 Sstop=0
 Bstop=0.1
 #Set pins for servo control
-finger1PinH=Button(7,pull_up=False)
-finger1PinL=Button(8,pull_up=False)
-finger2PinH=Button(11,pull_up=False)
-finger2PinL=Button(12,pull_up=False)
-finger3PinH=Button(15,pull_up=False)
-finger3PinL=Button(16,pull_up=False)
-finger4PinH=Button(21,pull_up=False)
-finger4PinL=Button(22,pull_up=False)
-finger5PinH=Button(23,pull_up=False)
-finger5PinL=Button(24,pull_up=False)
-wristLeftPinH=Button(31,pull_up=False)
-wristLeftPinL=Button(32,pull_up=False)
-wristRightPinH=Button(35,pull_up=False)
-wristRightPinL=Button(36,pull_up=False)
-wristHorizontalPinH=Button(37,pull_up=False)
-wristHorizontalPinL=Button(38,pull_up=False)
-elbowPinH=Button(26,pull_up=False)
-elbowPinL=Button(29,pull_up=False)
+#middle,ring and pinky fingers are controlled together
+fingergroupPinH=Button(7,pull_up=False)
+fingergroupPinL=Button(8,pull_up=False)
+#index finger is controlled separately
+fingerindexPinH=Button(11,pull_up=False)
+fingerindexPinL=Button(12,pull_up=False)
+fingerThumbPinH=Button(15,pull_up=False)
+fingerThumbPinL=Button(16,pull_up=False)
+#wrist has 3 degrees of freedom, each controlled separately
+wristLeftPinH=Button(21,pull_up=False)
+wristLeftPinL=Button(22,pull_up=False)
+wristRightPinH=Button(23,pull_up=False)
+wristRightPinL=Button(24,pull_up=False)
+wristHorizontalPinH=Button(31,pull_up=False)
+wristHorizontalPinL=Button(32,pull_up=False)
+
+elbowPinH=Button(35,pull_up=False)
+elbowPinL=Button(36,pull_up=False)
 time.sleep(1)
 while True:
-    f.setServoThrottleFilter(finger1, finger1PinH.is_pressed, finger1PinL.is_pressed,"small")
-    f.setServoThrottleFilter(finger2, finger2PinH.is_pressed, finger2PinL.is_pressed,"small")
-    f.setServoThrottleFilter(finger3, finger3PinH.is_pressed, finger3PinL.is_pressed,"small")
-    f.setServoThrottleFilter(finger4, finger4PinH.is_pressed, finger4PinL.is_pressed,"small")
-    f.setServoThrottleFilter(finger5, finger5PinH.is_pressed, finger5PinL.is_pressed,"small")
+    #move fingers:pinky, ring and middle fingers
+    f.setServoThrottleFilter(finger1, fingergroupPinH.is_pressed, fingergroupPinL.is_pressed,"small")
+    f.setServoThrottleFilter(finger2, fingergroupPinH.is_pressed, fingergroupPinL.is_pressed,"small")
+    f.setServoThrottleFilter(finger3, fingergroupPinH.is_pressed, fingergroupPinL.is_pressed,"small")
+    #move fingers: index and thumb fingers
+    f.setServoThrottleFilter(finger4, fingerindexPinH.is_pressed, fingerindexPinL.is_pressed,"small")
+    f.setServoThrottleFilter(finger5, fingerThumbPinH.is_pressed, fingerThumbPinL.is_pressed,"small")
+
     f.setServoThrottleFilter(wristLeft, wristLeftPinH.is_pressed, wristLeftPinL.is_pressed,"large")
     f.setServoThrottleFilter(wristRight, wristRightPinH.is_pressed, wristRightPinL.is_pressed,"large")
     f.setServoThrottleFilter(wristHorizontal, wristHorizontalPinH.is_pressed, wristHorizontalPinL.is_pressed,"large")
--- functions.py
+++ functions.py
@@ -4,11 +4,12 @@ from gpiozero import MCP3008, DigitalInputDevice
 
 
 def setServoThrottleFilter(servo, pin1, pin2,servoType):
+    """Sets the throttle of a servo based on the state of two pins"""
     if pin1 == True and pin2 == False:
         servo.throttle = 1
     elif pin1 == False and pin2 == True:
         servo.throttle = -1
-    else:
+    else:#large servos need a small amount of throttle to hold position, small servos can be stopped completely
         if servoType == "large":
             servo.throttle = 0.1
         else:
