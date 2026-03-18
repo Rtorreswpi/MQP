9dccdbc93be526bb4fc04e67341707c7f8b34866
Rtorreswpi <163170368+Rtorreswpi@users.noreply.github.com>
2026-03-18 12:34:04 -0400

    skeleton/pseudocode for teensy

--- sketch_mar16a/sketch_mar16a.ino
+++ sketch_mar16a/sketch_mar16a.ino
@@ -4,14 +4,19 @@ void setup() {
 }
 
 void loop() { 
+  // if(digitalRead(A10)>200)
+  //   digitalWrite(A6,High)
+  // }
   analogWrite(13, 255); // sets the LED on
   digitalWrite(A6,HIGH);
+  digitalWrite(A7,LOW);
   delay(3000);                // waits for a second (1000 ms)
   int sensorValue = digitalRead(A6); // Read analog pin A0
   Serial.println(sensorValue);      // Print value to Plotter
   analogWrite(13, 0);  // sets the LED off
   digitalWrite(A6,LOW);
-  delay(3000);
-  sensorValue = digitalRead(A6); // Read analog pin A0
-  Serial.println(sensorValue);      // Print value to Plotter
+  digitalWrite(A7,HIGH)
+  // delay(3000);
+  // sensorValue = digitalRead(A6); // Read analog pin A0
+  // Serial.println(sensorValue);      // Print value to Plotter
 }
