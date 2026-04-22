b33b3dcd371ba4651927eda91b94bd1564443b63
Rtorreswpi <Rtorres@wpi.edu>
2026-04-22 13:49:30 -0400

    more editss

--- test.py
+++ test.py
@@ -3,17 +3,15 @@ from adafruit_servokit import ServoKit
 from gpiozero import Button, DigitalInputDevice
 from signal import pause
 import functions as f
-
+kit = ServoKit(channels=16)
 pinA6 = DigitalInputDevice(4, pull_up=True)
 pinA7 = DigitalInputDevice(14, pull_up=True)
 finger1=kit.continuous_servo[0]#pinky
-finger1.throttle=0
-finger2=kit.continuous_servo[1]#ring
-finger2.throttle=0
+finger1.throttle=0.1
 while True:
     finger1.throttle=1
-    time.sleep(0.5)
+    time.sleep(1)
     finger1.throttle=-1
-    time.sleep(0.5)
-    finger1.throttle=0
-    time.sleep(0.5)
+    time.sleep(1)
+    finger1.throttle=0.1
+    time.sleep(1)
